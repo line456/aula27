0bfa6ee02f386ce283ebcedbcdb6aba77f40bbdf
unknown <aline.s.bezerra@aluno.senai.br>
2024-11-27 10:47:29 -0500

    logica de aprovação , recuperação e reprovação

--- git.py
+++ git.py
@@ -3,7 +3,13 @@ nota2 = float(input())
 nota3 = float(input())
 nota4 = float(input())
 
+faltas = int (input("digite o tanto de falta "))
+
 media = (nota1+nota2+nota3+nota4)/4
 
-if media >= 8:
+if media >= 8 and faltas <=30  :
     print("o aluno esta aprovado")
+elif media >= 5 and faltas <=30:
+    print("o aluno esta de recuperação")
+else: 
+    print("reprovado")
