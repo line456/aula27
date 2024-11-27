dfcf80da015a536f7141cf8e5c67def92b4aaea9
unknown <aline.s.bezerra@aluno.senai.br>
2024-11-27 10:29:57 -0500

    logicas de umas nota de alunos

---
+++ git.py
@@ -0,0 +1,9 @@
+nota1 = float (input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+
+media = (nota1+nota2+nota3+nota4)/4
+
+if media >= 8:
+    print("o aluno esta aprovado")
