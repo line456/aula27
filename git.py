cd98df777dd195b09dc6c3f12766dcf00317c05f
Aline Sousa <aline.s.bezerra@aluno.senai.br>
2024-12-03 10:33:56 -0500

    fim do menu

--- git.py
+++ git.py
@@ -1,5 +1,7 @@
 alunos = [] # alunos 
 notas = [] # notas dos alunos 
+nota1 = nota2 = nota3 = nota4 = media = 0.0
+# menu de situação de alunos 
 while True: 
     print("1.cadastro, 2.ver relatorio, 3.encerrar ") 
     decisao = input() #numero de cadastro
@@ -8,14 +10,15 @@ while True:
         quantidadealuno = int (input(" quantidade de aluno "))
         cont=0
         situacao = ""
-        while cont <= quantidadealuno:
-            nome = input("nome ")
+        while cont < quantidadealuno:
+            nome = input("nome do aluno ")
             #LER AS NOTAS E FALTAS 
             faltas = int(input("quantidade de faltas "))
-            for cont in range(4):
-                nota = float (input("nota1 "))
-                notas.append(nota)
-            
+            # 4 notas 
+            for i in range(4):
+                notas.append(float (input("nota ")))
+                
+            #calculo da media 
             media = (notas[0]+notas[1]+notas[2]+notas[3])/4
             #APROVADO , RECUPERAÇÃO E REPROVADO 
             if media >= 8 and faltas <=30  :
@@ -31,10 +34,19 @@ while True:
             else: 
                     situacao = "reprovado"
             
-            alunos.append([nome, notas, faltas, media, situacao])
+            alunos.append([nome, faltas, notas, media, situacao]) #guardando
+            notas = []
             cont += 1
+    # relatorio 
     elif decisao == "2":
-        print(alunos)
+        for i in alunos:
+            print(f"Nome do aluno  {i[0]}")
+            print (f"Faltas: {i[1]}")
+            print (f"notas: {', '.join(map(str, i[2]))}") 
+            print (f"media: {i[3]}")
+            print(f"situação: {i[4]}")
+    #encerramento 
     else :
         print("fim")
-    
+        break
+        
