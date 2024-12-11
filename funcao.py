4915796e9560aadf6c64ff2bd4767759fb7e15b7
aline <aline.s.bezerra@aluno.senai>
2024-12-11 08:46:53 -0500

    criando funções

---
+++ funcao.py
@@ -0,0 +1,13 @@
+#declarar uma função
+def bom_dia():
+    print("BOM DIA! ")
+
+def boa_noite():
+    print("boa noite!")
+
+def boa_tarde ():
+    print("boa tarde ")
+    
+bom_dia()
+boa_tarde()
+boa_noite()
